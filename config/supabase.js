1f24ac146491d9471608621d57685c2cad6513bf
MonzFourYou <hasibuanhamonangan43@gmail.com>
2026-07-22 20:50:42 +0700

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,1 +1,24 @@
- 
+/*
+=====================================
+ Astrava Panel
+ Supabase Configuration
+=====================================
+*/
+
+const SUPABASE = {
+
+    URL: "",
+
+    ANON_KEY: ""
+
+};
+
+/*
+Contoh nanti
+
+URL:
+https://xxxxxxxx.supabase.co
+
+ANON_KEY:
+eyJhbGc....
+*/ 
