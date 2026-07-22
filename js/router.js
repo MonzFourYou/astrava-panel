42d31b4d0fef739b888a6a77c8aa3f8df9f0b4e9
MonzFourYou <hasibuanhamonangan43@gmail.com>
2026-07-22 20:31:24 +0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,13 @@
+const menu = document.querySelectorAll(".menu-item");
 
+menu.forEach(item => {
+
+    item.addEventListener("click", () => {
+
+        menu.forEach(i => i.classList.remove("active"));
+
+        item.classList.add("active");
+
+    });
+
+});
