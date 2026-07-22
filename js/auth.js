37468b84c03c5438d7006c554d93086bd7d2d18d
MonzFourYou <hasibuanhamonangan43@gmail.com>
2026-07-22 20:29:30 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,22 @@
+const form = document.getElementById("loginForm");
 
+form.addEventListener("submit",(e)=>{
+
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username==="admin" && password==="admin"){
+
+        alert("Login Berhasil!");
+
+        window.location.href="pages/dashboard.html";
+
+    }else{
+
+        alert("Username atau Password Salah!");
+
+    }
+
+});
