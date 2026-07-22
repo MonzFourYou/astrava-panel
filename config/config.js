e449e9a9c15b2e6ff77d0a5efb1318d1f761897b
MonzFourYou <hasibuanhamonangan43@gmail.com>
2026-07-22 20:50:23 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,22 @@
- 
+/*
+=====================================
+ Astrava Panel
+ Global Configuration
+=====================================
+*/
+
+const CONFIG = {
+
+    APP_NAME: "Astrava Panel",
+
+    VERSION: "1.0.0",
+
+    AUTHOR: "MonzFourYou",
+
+    DOMAIN: "",
+
+    API_URL: "",
+
+    DEBUG: true
+
+}; 
